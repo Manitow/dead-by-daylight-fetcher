e7314713d26a1c4f0e02243142d7ea497c4b169d
Manitow <99738556+Manitow@users.noreply.github.com>
2021-05-12 11:02:37 +0000

    Minor doc updates

---
+++ Blockchain/task/src/blockchain/Blockchain.java
@@ -0,0 +1,15 @@
+package blockchain;
+
+import blockchain.block.Block;
+import blockchain.block.PartBlockParams;
+import blockchain.data.Data;
+import blockchain.data.DataParams;
+import blockchain.hash.HashApprover;
+import blockchain.reward.Reward;
+
+public interface Blockchain<B extends Block, D extends Data> {
+    HashApprover getApprover();
+
+    PartBlockParams getNextBlockParams();
+
+    
