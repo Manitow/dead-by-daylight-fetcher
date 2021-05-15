7281c18f41b468208568d5e0f0d92cf06a8f71a2
Manitow <99738556+Manitow@users.noreply.github.com>
2021-05-15 09:39:52 +0000

    improved connection and / or JTA transaction utilities

--- Blockchain/task/src/blockchain/Main.java
+++ Blockchain/task/src/blockchain/Main.java
@@ -12,4 +12,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
-public class Main 
+public class Main {
+    private static final int NUMBER_OF_MINERS = 4;
+    private static final int NUMBER_OF_BLOCKS = 15;
+    private static final int NUMBER_OF_PRODUCERS = 5;
+    private static List<String> TEXTS = List.of(
+            "Hello",
+            "Hi",
+            "Byu",
+            "Yes",
+            "No"
+    );
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Blockchain<Block, SignedData> blockchain = new MyBlockchain();
+        final ExecutorServic
