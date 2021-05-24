602884ab9291c46612f4ec8afb06b8bbb2257b3a
Manitow <99738556+Manitow@users.noreply.github.com>
2021-05-24 16:47:21 +0000

    Issue #11

--- Blockchain/task/src/blockchain/MyBlockchain.java
+++ Blockchain/task/src/blockchain/MyBlockchain.java
@@ -6,4 +6,18 @@ import blockchain.block.NZerosBlock;
 import blockchain.block.PartBlockParams;
 import blockchain.data.Data;
 import blockchain.data.DataParams;
-import blockchain.data.ImmutableDa
+import blockchain.data.ImmutableDataParams;
+import blockchain.data.SignedData;
+import blockchain.hash.HashApprover;
+import blockchain.hash.NZerosHashApprover;
+import blockchain.reward.Reward;
+import blockchain.reward.VirtualCoin;
+import blockchain.util.NZeros;
+
+import java.util.*;
+
+public class MyBlockchain implements Blockchain<Block, SignedData> {
+    protected final Deque<Block> blocks;
+    protected final Deque<SignedData> dataSet;
+    protected final Object lock = new Object();
+    protect
