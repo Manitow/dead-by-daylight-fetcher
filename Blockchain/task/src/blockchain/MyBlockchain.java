0d5f5856a1697ff1d76c7978e84ae2891392580f
Manitow <99738556+Manitow@users.noreply.github.com>
2021-05-24 18:18:42 +0000

    ceph: messenger: update "to" in read_partial() caller

--- Blockchain/task/src/blockchain/MyBlockchain.java
+++ Blockchain/task/src/blockchain/MyBlockchain.java
@@ -20,4 +20,22 @@ public class MyBlockchain implements Blockchain<Block, SignedData> {
     protected final Deque<Block> blocks;
     protected final Deque<SignedData> dataSet;
     protected final Object lock = new Object();
-    protect
+    protected long blockId;
+    protected long dataId;
+    protected long reward;
+    protected NZeros nZeros;
+    protected NZerosHashApprover approver;
+    protected PartBlockParams blockParams;
+
+    public MyBlockchain() {
+        blocks = new ArrayDeque<>();
+        dataSet = new ArrayDeque<>();
+        blockId = 1;
+        dataId = 1;
+        reward = 100;
+        nZeros = new NZeros(0);
+        prepareNext();
+    }
+
+    protected void prepareNext() {
+        dataSet.
