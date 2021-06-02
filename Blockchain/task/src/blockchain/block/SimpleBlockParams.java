47361a557f28a321aa705cd7820c2f539157db9e
Manitow <99738556+Manitow@users.noreply.github.com>
2021-06-02 05:34:21 +0000

    Bumped version to 1.0.4

--- Blockchain/task/src/blockchain/block/SimpleBlockParams.java
+++ Blockchain/task/src/blockchain/block/SimpleBlockParams.java
@@ -4,4 +4,27 @@ import java.util.Objects;
 
 public class SimpleBlockParams implements BlockParams {
     protected long id;
-    protected long timestamp
+    protected long timestamp;
+    protected long runtime;
+    protected long magicNumber;
+    protected String hash;
+    protected String previousHash;
+    protected String owner;
+
+    public SimpleBlockParams(PartBlockParams params) {
+        this.id = params.getId();
+        this.previousHash = params.getPreviousHash();
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+ 
