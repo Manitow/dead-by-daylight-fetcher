f5835a06f2303d465a36683bb7c2e4421285ac4d
Manitow <99738556+Manitow@users.noreply.github.com>
2021-06-04 18:05:24 +0000

    repo fix

--- Blockchain/task/src/blockchain/data/Message.java
+++ Blockchain/task/src/blockchain/data/Message.java
@@ -1,3 +1,22 @@
 package blockchain.data;
 
-import ja
+import java.util.Objects;
+
+public class Message implements Data {
+    protected final long id;
+    protected final String text;
+
+    public Message(long id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return fa
