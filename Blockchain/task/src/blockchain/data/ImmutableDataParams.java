59ba74c92c172993806169965b7cb576a9a094a0
Manitow <99738556+Manitow@users.noreply.github.com>
2021-06-04 16:06:25 +0000

    bump to version 0.3.0

--- Blockchain/task/src/blockchain/data/ImmutableDataParams.java
+++ Blockchain/task/src/blockchain/data/ImmutableDataParams.java
@@ -15,4 +15,22 @@ public class ImmutableDataParams implements DataParams {
     }
 
     @Override
-    public boole
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ImmutableDataParams that = (ImmutableDataParams) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ImmutableDataParams{" +
+                "id=" + id +
+                '}';
+    }
+}
