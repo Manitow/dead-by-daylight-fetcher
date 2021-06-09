7e5cbde95a3df7fa8cf6a12d4482aaec147653a9
Manitow <99738556+Manitow@users.noreply.github.com>
2021-06-09 19:11:16 +0000

    Precompiled assets

--- Blockchain/task/src/blockchain/data/SignedMessageProducer.java
+++ Blockchain/task/src/blockchain/data/SignedMessageProducer.java
@@ -9,4 +9,14 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SignatureException;
 import java.util.List;
-impo
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class SignedMessageProducer extends MessageProducer<SignedMessage> {
+    protected final Supplier<DataParams> params;
+    protected final KeysGenerator keysGenerator;
+    protected final PublicKey publicKey;
+    protected final PrivateKey privateKey;
+
+    public SignedMessageProducer(String name, List<String> texts,
+                                 Consumer<SignedMessage> consumer, Supplier<DataParams> params
