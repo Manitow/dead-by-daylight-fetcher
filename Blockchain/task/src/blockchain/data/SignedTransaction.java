3eabad928a7f956e84e932831db624b6df09a76a
Manitow <99738556+Manitow@users.noreply.github.com>
2021-06-16 05:40:06 +0000

    TaskRunner in progress

--- Blockchain/task/src/blockchain/data/SignedTransaction.java
+++ Blockchain/task/src/blockchain/data/SignedTransaction.java
@@ -1,1 +1,16 @@
-packa
+package blockchain.data;
+
+import blockchain.reward.Reward;
+
+import java.security.PublicKey;
+
+public class SignedTransaction implements Transaction, SignedData {
+    private final long id;
+    private final String from;
+    private final String to;
+    private final Reward reward;
+    private final String sign;
+    private final PublicKey key;
+
+    public SignedTransaction(long id, String from, String to, Reward reward, String sign, PublicKey key) {
+        this.i
