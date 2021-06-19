c8a149bb43433b8f306d2593f2c8f27cee45fb67
Manitow <99738556+Manitow@users.noreply.github.com>
2021-06-19 16:44:03 +0000

    Fixing some errors

--- Blockchain/task/src/blockchain/mining/ComputeMiner.java
+++ Blockchain/task/src/blockchain/mining/ComputeMiner.java
@@ -1,4 +1,18 @@
 package blockchain.mining;
 
 import blockchain.block.Block;
-import blockchain.block.ImmutableBloc
+import blockchain.block.ImmutableBlock;
+import blockchain.block.PartBlockParams;
+import blockchain.block.SimpleBlockParams;
+import blockchain.hash.HashApprover;
+import blockchain.hash.HashFunction;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+import static blockchain.util.CommonUtils.getTimestamp;
+
+public class ComputeMiner implements Miner, Callable<Block> {
+    private final String name;
+    private final HashFunction hashFunction;
+    private final HashApprover hashAppro
