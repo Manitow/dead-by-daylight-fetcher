0b29e0d22743256a32b2084a8264bb474c3ec831
Manitow <99738556+Manitow@users.noreply.github.com>
2021-06-20 21:55:46 +0000

    Added that feature we discussed on... Was it monday?

--- Blockchain/task/src/blockchain/mining/MinerParams.java
+++ Blockchain/task/src/blockchain/mining/MinerParams.java
@@ -1,1 +1,15 @@
-package blockc
+package blockchain.mining;
+
+import blockchain.block.PartBlockParams;
+import blockchain.hash.HashApprover;
+import blockchain.hash.HashFunction;
+
+public interface MinerParams {
+    String getName();
+
+    HashFunction getHashFunction();
+
+    HashApprover getHashApprover();
+
+    PartBlockParams getBlockParams();
+}
---
+++ Blockchain/task/src/blockchain/mining/SimpleMinerParams.java
@@ -0,0 +1,7 @@
+package blockchain.mining;
+
+import blockchain.block.PartBlockParams;
+import blockchain.hash.HashApprover;
+import blockchain.hash.HashFunction;
+
+public clas
