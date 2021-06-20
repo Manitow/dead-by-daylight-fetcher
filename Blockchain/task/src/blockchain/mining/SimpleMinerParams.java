ec121a9d5ffa2fca44c75f48922db879cbebf52c
Manitow <99738556+Manitow@users.noreply.github.com>
2021-06-20 22:04:02 +0000

    Edited release notes.

--- Blockchain/task/src/blockchain/mining/SimpleMinerParams.java
+++ Blockchain/task/src/blockchain/mining/SimpleMinerParams.java
@@ -4,4 +4,24 @@ import blockchain.block.PartBlockParams;
 import blockchain.hash.HashApprover;
 import blockchain.hash.HashFunction;
 
-public clas
+public class SimpleMinerParams implements MinerParams {
+    private String name;
+    private HashFunction hashFunction;
+    private HashApprover hashApprover;
+    private PartBlockParams blockParams;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public HashFunction getHashFunction() {
+        return hashFunction;
+    }
+
+    public void setHashFunction(HashFunction hashFunc
