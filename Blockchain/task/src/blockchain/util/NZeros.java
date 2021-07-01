8b8043f0f0a32b96ef7d846cab5a62a88672e8a3
Manitow <99738556+Manitow@users.noreply.github.com>
2021-07-01 10:06:00 +0000

    Import fixes.

--- Blockchain/task/src/blockchain/util/NZeros.java
+++ Blockchain/task/src/blockchain/util/NZeros.java
@@ -19,4 +19,17 @@ public class NZeros {
         return count;
     }
 
-    public Stri
+    public String getNextStatus() {
+        int variant = random.nextInt(10);
+        if (count > 0 && variant < 3) {
+            --count;
+            increasedCount = 0;
+            return "N was decreased by " + ++decreasedCount;
+        } else if (count < 4 && variant > 6) {
+            ++count;
+            decreasedCount = 0;
+            return "N was increased to " + ++increasedCount;
+        } else {
+            increasedCount = 0;
+            decreasedCount = 0;
+         
