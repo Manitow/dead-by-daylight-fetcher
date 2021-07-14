8f6df1ab1046e1dbe5f4e38a135fc8635bf52a1f
Manitow <99738556+Manitow@users.noreply.github.com>
2021-07-14 20:15:17 +0000

    merge fix from RC_0_16

---
+++ Problems/Find all mondays/src/Main.java
@@ -0,0 +1,8 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+      
