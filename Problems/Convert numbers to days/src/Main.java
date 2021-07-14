bad7fdf01fdffb61796cc51743134941804bbacd
Manitow <99738556+Manitow@users.noreply.github.com>
2021-07-14 10:58:18 +0000

    Prepare 0.0.3 release

--- Problems/Convert numbers to days/src/Main.java
+++ Problems/Convert numbers to days/src/Main.java
@@ -6,4 +6,10 @@ class Main {
         Scanner scanner = new Scanner(System.in);
         int year = scanner.nextInt();
         int one = scanner.nextInt();
-        int
+        int two = scanner.nextInt();
+        int three = scanner.nextInt();
+        System.out.println(LocalDate.ofYearDay(year, one));
+        System.out.println(LocalDate.ofYearDay(year, two));
+        System.out.println(LocalDate.ofYearDay(year, three));
+    }
+}
