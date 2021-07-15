da7ea63fcb977886ca83cb59a6f7db66e3507f1e
Manitow <99738556+Manitow@users.noreply.github.com>
2021-07-15 18:08:22 +0000

    ultimo

--- Problems/Find all mondays/src/Main.java
+++ Problems/Find all mondays/src/Main.java
@@ -5,4 +5,13 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-      
+        int year = scanner.nextInt();
+        int month = scanner.nextInt();
+        LocalDate date = LocalDate.of(year, month, 1);
+        for (LocalDate d = date; d.getMonth() == d.plusDays(1).getMonth(); d = d.plusDays(1)) {
+            if (d.getDayOfWeek() == DayOfWeek.MONDAY) {
+                System.out.println(d);
+            }
+        }
+    }
+}
