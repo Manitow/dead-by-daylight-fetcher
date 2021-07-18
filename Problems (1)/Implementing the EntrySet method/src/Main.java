6ed56ef42cbc019a7c48e6969ef29973b6be750d
Manitow <99738556+Manitow@users.noreply.github.com>
2021-07-18 15:44:12 +0000

    n/a

--- Problems (1)/Implementing the EntrySet method/src/Main.java
+++ Problems (1)/Implementing the EntrySet method/src/Main.java
@@ -1,1 +1,15 @@
-impor
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        HashTable<String> table = new HashTable<>(count);
+        for (int i = 0; i < count; i++) {
+            int key = scanner.nextInt();
+            String value = scanner.nextLine();
+            table.put(key, value);
+        }
+        for (TableE
