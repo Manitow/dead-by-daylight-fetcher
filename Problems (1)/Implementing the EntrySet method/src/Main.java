d1591aff14f2bf01853732361c9942d9d5f94771
Manitow <99738556+Manitow@users.noreply.github.com>
2021-07-19 09:55:33 +0000

    init

--- Problems (1)/Implementing the EntrySet method/src/Main.java
+++ Problems (1)/Implementing the EntrySet method/src/Main.java
@@ -12,4 +12,27 @@ public class Main {
             String value = scanner.nextLine();
             table.put(key, value);
         }
-        for (TableE
+        for (TableEntry<String> entry : table.entrySet()) {
+            System.out.printf("%d: %s%n", entry.getKey(), entry.getValue());
+        }
+    }
+
+    private static class TableEntry<T> {
+        private final int key;
+        private final T value;
+
+        public TableEntry(int key, T value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public T getValue() {
+            return value;
+        }
+    }
+
+   
