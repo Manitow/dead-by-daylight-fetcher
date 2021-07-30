6d6cdb3dec4b520d79382069f33527df0e5b3178
Manitow <99738556+Manitow@users.noreply.github.com>
2021-07-30 05:47:50 +0000

    perf/x86/amd/ibs: Fix waking up from S3 for AMD family 10h

--- Problems (10)/Rehashing/src/Main.java
+++ Problems (10)/Rehashing/src/Main.java
@@ -2,4 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner =
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        HashTable<String> hashTable = new HashTable<>(5);
+        for (int i = 0; i < size; i++) {
+            int key = scanner.nextInt();
+            String value = scanner.nextLine().trim();
+            hashTable.put(key, value);
+        }
+        System.out.println(hashTable);
+    }
+
+    private static class TableEntry<T> {
+        private final int key;
+        private final T value;
+
+        public Tab
