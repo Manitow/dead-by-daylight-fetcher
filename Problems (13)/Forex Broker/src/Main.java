940bdf05f439a232ca1f6e7d8f3f0a2932dbe3a7
Manitow <99738556+Manitow@users.noreply.github.com>
2021-09-01 18:58:27 +0000

    USB: gadget: android: android USB gadget improvements:

--- Problems (13)/Forex Broker/src/Main.java
+++ Problems (13)/Forex Broker/src/Main.java
@@ -39,4 +39,34 @@ class Option {
     }
 
     void buy() {
-        System.out.println(amount + " was bou
+        System.out.println(amount + " was bought");
+    }
+
+    void sell() {
+        System.out.println(amount + " was sold");
+    }
+}
+
+class BuyCommand implements Command {
+    private Option option;
+
+    BuyCommand(Option option) {
+        this.option = option;
+    }
+
+    @Override
+    public void execute() {
+        option.buy();
+    }
+}
+
+class SellCommand implements Command {
+    private Option option;
+
+    SellCommand(Option option) {
+        this.option = option;
+    }
+
+    @Override
+    public void execute() {
+        op
