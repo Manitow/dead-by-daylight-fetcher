e1422eb7c22a6ec45a0eb0bb4578c7a6c4d7cf99
Manitow <99738556+Manitow@users.noreply.github.com>
2021-09-10 22:09:01 +0000

    Update builds

--- Problems (15)/Different commands/src/Main.java
+++ Problems (15)/Different commands/src/Main.java
@@ -12,3 +12,30 @@ interface Storable {
     int getInventoryLength();
 
     String getInventoryItem(int index);
+
+    void setInventoryItem(int index, String item);
+}
+
+interface Command {
+    void execute();
+}
+
+class CommandMove implements Command {
+    Movable entity;
+    int xMovement;
+    int yMovement;
+
+    @Override
+    public void execute() {
+        entity.setX(xMovement);
+        entity.setY(yMovement);
+    }
+}
+
+class CommandPickItem implements Command {
+    Storable entity;
+    String item;
+
+    @Override
+    public void execute() {
+        for (int i = 0; i < entity
