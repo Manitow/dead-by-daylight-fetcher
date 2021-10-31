e0a953ecf085828369e313f7e6c4d84c0a95cab6
Manitow <99738556+Manitow@users.noreply.github.com>
2021-10-31 21:06:26 +0000

    Update (^_^)(_ _).md

--- Problems (2)/Phone Book/src/Main.java
+++ Problems (2)/Phone Book/src/Main.java
@@ -6,4 +6,14 @@ public class Main {
         int count = scanner.nextInt();
         HashTable<String> table = new HashTable<>(count);
         for (int i = 0; i < count; i++) {
-            Str
+            String command = scanner.next();
+            if ("put".equals(command)) {
+                int key = scanner.nextInt();
+                String value = scanner.nextLine().trim();
+                table.put(key, value);
+            } else if ("get".equals(command)) {
+                int key = scanner.nextInt();
+                String value = table.get(key);
+                if (value != null) {
+                    System.out.println(value);
+                } e
