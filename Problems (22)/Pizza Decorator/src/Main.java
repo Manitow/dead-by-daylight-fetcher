4e166cf0757bf008273564bd3863ac25d4d9a5ee
Manitow <99738556+Manitow@users.noreply.github.com>
2021-12-23 16:36:22 +0000

    Fixed compilation errors

---
+++ Problems (22)/Pizza Decorator/src/Main.java
@@ -0,0 +1,24 @@
+
+import java.util.Locale;
+
+class TestDrive {
+    public static void main(String[] args) {
+        Pizza simpleVeganPizza = new Vegan();
+        System.out.println(simpleVeganPizza.getDescription() + " $" + formatSum(simpleVeganPizza.cost()));
+
+        Pizza veganPizzaDecor = new Vegan();
+        veganPizzaDecor = new Broccoli(veganPizzaDecor);
+        veganPizzaDecor = new Tomato(veganPizzaDecor);
+        veganPizzaDecor = new Spinach(veganPizzaDecor);
+        System.out.println(veganPizzaDecor.getDescription() + " $" + formatSum(veganPizzaDecor.cost()));
+
+        Pizza meatPizzaDecor = new MeatHeaven();
+        meatPizzaDecor = new Ham(meatPizzaDecor);
+        meatPizzaDecor = new Chicken(meatPizzaDecor);
+        meatPizzaDecor = new Cheese(meatPizzaDecor);
+        System.out.println(meatPizzaDecor.getDescription() + " $" + formatSum(meatPizzaDecor.cost()));
+
+    }
+
+    private static String formatSum(double sum) {
+        return String.format(Locale.ROOT, "%.2f", sum);
