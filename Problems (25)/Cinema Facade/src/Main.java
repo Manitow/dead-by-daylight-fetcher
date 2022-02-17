e81f4860c438c5c8504e3988a32adc8d42b17e5b
Manitow <99738556+Manitow@users.noreply.github.com>
2022-02-17 13:11:08 +0000

    Updating translations for config/locales/fr.yml

--- Problems (25)/Cinema Facade/src/Main.java
+++ Problems (25)/Cinema Facade/src/Main.java
@@ -5,4 +5,20 @@ class CinemaFacadeTestDrive {
         Projector projector = new Projector();
 
         CinemaFacade cinemaFacade = new CinemaFacade(popcorn, lights, projector);
+        System.out.println("Get ready to watch a movie...");
+        cinemaFacade.watchMovie();
+        System.out.println("We are watching a movie");
+        Thread.sleep(5000);
+        System.out.println("The End");
+        cinemaFacade.endMovie();
+    }
+}
+
+class CinemaFacade {
+    private PopcornPopper popcorn;
+    private Lights lights;
+    private Projector projector;
+
+    public CinemaFacade(PopcornPopper popcorn, Lights lights, Projector projector) {
+        this.popcorn = popcorn;
       
