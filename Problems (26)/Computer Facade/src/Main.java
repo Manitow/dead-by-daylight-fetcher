257a33c3d0aedec7ae25b06992673b309a66f068
Manitow <99738556+Manitow@users.noreply.github.com>
2022-02-19 13:31:19 +0000

    Update global.css

--- Problems (26)/Computer Facade/src/Main.java
+++ Problems (26)/Computer Facade/src/Main.java
@@ -2,4 +2,19 @@ class ComputerFacadeTestDrive {
     public static void main(String[] args) {
         Processor processor = new Processor();
         Monitor monitor = new Monitor();
- 
+        Keyboard keyboard = new Keyboard();
+
+        ComputerFacade computerFacade = new ComputerFacade(processor, monitor, keyboard);
+
+        computerFacade.turnOnComputer();
+        computerFacade.turnOffComputer();
+    }
+}
+
+class ComputerFacade {
+    private final Processor processor;
+    private final Monitor monitor;
+    private final Keyboard keyboard;
+
+    ComputerFacade(Processor processor, Monitor monitor, Keyboard keyboard) {
+        this.processor = proc
