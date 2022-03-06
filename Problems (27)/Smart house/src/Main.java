0502e921abf0e1d3f99dd05a9f46d393a3adda29
Manitow <99738556+Manitow@users.noreply.github.com>
2022-03-06 18:29:26 +0000

    Delete private.properties

--- Problems (27)/Smart house/src/Main.java
+++ Problems (27)/Smart house/src/Main.java
@@ -8,4 +8,20 @@ class SmartHouseFacadeTestDrive {
 
         lights.setFavouriteСolorTemperature("Calming blue");
         stereoSystem.setFavouriteSong("Queen - Killer Queen");
-     
+        bathroom.setFavouriteTemperature("35℃");
+        bathroom.setFavouriteLevel("60%");
+
+        smartHouseFacade.cameHome();
+        smartHouseFacade.leaveBathroomGoSleep();
+    }
+}
+
+class SmartHouseFacade {
+    StereoSystem stereoSystem;
+    Bathroom bathroom;
+    Lights lights;
+
+    public SmartHouseFacade(StereoSystem stereoSystem, Bathroom bathroom, Lights lights) {
+        this.stereoSystem = stereoSystem;
+        this.bathroom = bathroom;
+        this.lights = lights;
