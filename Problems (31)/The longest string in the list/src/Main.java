3171a95b58fa49435f2a45fe8f040e6a7fb7368b
Manitow <99738556+Manitow@users.noreply.github.com>
2022-04-13 11:38:45 +0000

    Update sbt-release to 1.0.13

--- Problems (31)/The longest string in the list/src/Main.java
+++ Problems (31)/The longest string in the list/src/Main.java
@@ -7,4 +7,20 @@ public class Main {
     static void changeList(List<String> list) {
         if (list.isEmpty()) {
             return;
- 
+        }
+        int size = list.size();
+        String longestString = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            String string = list.get(i);
+            if (string.length() > longestString.length()) {
+                longestString = string;
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            list.set(i, longestString);
+        }
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = 
