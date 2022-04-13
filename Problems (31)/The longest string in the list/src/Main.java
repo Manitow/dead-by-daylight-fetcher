67efae7fa3606dce4f5fc9af16730444a3f8aac7
Manitow <99738556+Manitow@users.noreply.github.com>
2022-04-13 10:49:13 +0000

    state applied

---
+++ Problems (31)/The longest string in the list/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    static void changeList(List<String> list) {
+        if (list.isEmpty()) {
+            return;
+ 
