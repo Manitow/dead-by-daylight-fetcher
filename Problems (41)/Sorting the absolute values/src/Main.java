612a9e5bdc60f2b595bb61e0eaa2a56f19f94432
Manitow <99738556+Manitow@users.noreply.github.com>
2022-07-30 15:47:51 +0000

    msm: perf: Protect buffer overflow due to malicious user

--- Problems (41)/Sorting the absolute values/src/Main.java
+++ Problems (41)/Sorting the absolute values/src/Main.java
@@ -4,4 +4,14 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String result = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::valueOf)
+                .mapToInt(Math::abs)
+                .sorted()
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "));
+        System.out.println(result);
+    }
+}
