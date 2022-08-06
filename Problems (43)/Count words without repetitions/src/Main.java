1959aab99702ac07e16521cf0e7f31f16ce66d3e
Manitow <99738556+Manitow@users.noreply.github.com>
2022-08-06 17:53:38 +0000

    Replaced static WSDL code with call to getXMLFragments

---
+++ Problems (43)/Count words without repetitions/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        System.out.println(Stream.generate(scanner::nextLine)
+                .limit(count + 1)
+                .flatMap(s -> Arrays.stream(s.spl
