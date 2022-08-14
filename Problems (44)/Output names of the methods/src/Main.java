5ae085aaa1f2f2087904ff549cf13eccc82361fb
Manitow <99738556+Manitow@users.noreply.github.com>
2022-08-14 08:47:38 +0000

    code mort

---
+++ Problems (44)/Output names of the methods/src/Main.java
@@ -0,0 +1,10 @@
+
+SuperClass instance = new SuperClass() {
+    public void method2() {
+        System.out.println("method2");
+    }
+
+    public void method3() {
+        System.out.println("method3");
+    }
+};
