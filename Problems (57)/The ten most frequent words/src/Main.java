5ab75c623ba4d34a7c3e901c8587c989c2830819
Manitow <99738556+Manitow@users.noreply.github.com>
2022-12-20 20:26:40 +0000

    Even more cleanup

---
+++ Problems (57)/The ten most frequent words/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+    publi
