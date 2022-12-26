ba6edb9c70af74f6aa5b74a92927d3294c5e2bcb
Manitow <99738556+Manitow@users.noreply.github.com>
2022-12-26 12:03:44 +0000

    Code review markups

--- Problems (58)/Secret codes/src/Main.java
+++ Problems (58)/Secret codes/src/Main.java
@@ -2,4 +2,19 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
-i
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    private static List<String> extractCodes(List<String> codes) {
+        return codes.stream()
+                .dropWhile(Predicate.not("#0000"::equals))
+                .skip(1)
+                .takeWhile(Predicate.not("#FFFF"::equals))
+                .collect(Collectors.toList());
+    }
+
+    /* Please do not modify the code below */
+    public static void main(String[] args) {
+        S
