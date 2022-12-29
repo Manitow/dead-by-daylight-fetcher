219eea4210cdf791a0a832bc29142eb599f884fb
Manitow <99738556+Manitow@users.noreply.github.com>
2022-12-29 10:47:49 +0000

    Type: A Descr: retouche

--- Problems (59)/Inverting streams/src/Main.java
+++ Problems (59)/Inverting streams/src/Main.java
@@ -5,4 +5,19 @@ public class Main {
 
     private static List<LongStream> invertedStreams(List<LongStream> streams) {
        return streams.stream()
-                .map(s -> s.isParallel() ? s.sequential() : s.parall
+                .map(s -> s.isParallel() ? s.sequential() : s.parallel())
+                .collect(Collectors.toList());
+    }
+
+    /* Do not modify the code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Boolean> parallelFlags = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .map(Boolean::parseBoolean)
+                .collect(Collectors.toList());
+
+        // :)
+        List<LongStream> streams = Stream
+               
