2d69038794354f96256493429ce7c65bcf0f9981
Manitow <99738556+Manitow@users.noreply.github.com>
2022-12-29 16:18:14 +0000

    con

--- Problems (6)/Burgers/src/Main.java
+++ Problems (6)/Burgers/src/Main.java
@@ -3,4 +3,14 @@ class TestDrive {
         BurgerStore mcDonalds = new McDonStore();
         BurgerStore burgerKing = new BurgerKingStore();
 
-   
+        Burger burger;
+
+        System.out.println("-Hello, one McDonalds style cheeseburger");
+        System.out.println("-Okay! Please wait for a sec, -Calling to the McDonaldsStore. -Cheeseburger");
+        burger = mcDonalds.orderBurger(Burger.CHEESE);
+        Thread.sleep(1500);
+        System.out.println("There it is!");
+        System.out.println(burger.getName() + "\n");
+
+        System.out.println("-Hello, one BurgerKing style hamburger");
+        System.out.println("-Okay! P
